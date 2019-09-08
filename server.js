faa166c2b022e2502eed63e1be82c58c1eca214a
danielbissell <40673531+danielbissell@users.noreply.github.com>
2019-09-08 16:39:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const Datastore = require('nedb');
 const request = require('request');
 var async = require("async");
-const key = 'xxx';
+const key = ENV['api_key'];
 
 const app = express();
 
